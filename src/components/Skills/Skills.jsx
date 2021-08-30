53c89925377aa09b79fbe831bc60683d815629b9
Chris Johnston <christopherjohnston@protonmail.com>
2021-08-30 22:36:40 +0100

    Added skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import Fade from 'react-reveal/Fade';
+
 import { Container } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Fade from 'react-reveal/Fade';
+
+import Title from '../Title/Title';
+
+// Technology icons
+import css from '../../images/techicons/CSS3_logo_and_wordmark.svg';
+import html from '../../images/techicons/HTML5_logo_and_wordmark.svg';
+import js from '../../images/techicons/Javascript-shield.svg';
+import react from '../../images/techicons/React-icon.svg';
 
 const skillItems = [
-  { skillName: 'React' },
-  { skillName: 'Javascript' },
-  { skillName: 'HTML' },
-  { skillName: 'CSS' },
+  { skillName: 'React', subSkills: ['Node.js', 'Gatsby'], logo: react },
+  { skillName: 'Javascript', subSkills: ['JSX', 'ES6'], logo: js },
+  { skillName: 'HTML', logo: html },
+  { skillName: 'CSS', subSkills: ['SCSS'], logo: css },
 ];
 
 const Skills = () => {
@@ -25,14 +35,25 @@ const Skills = () => {
 
   return (
     <section id="skills">
-      <Container>
+      <Container className="skills-container">
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1>Skills</h1>
-          <ul>
+          <Title title="Skills" />
+          <ListGroup horizontal className="skill-items">
             {skillItems.map((skill) => (
-              <li>{skill.skillName}</li>
+              <>
+                <ListGroup.Item>
+                  <img src={skill.logo} width="100%" height="50px" alt={skill.Skillname} />
+                  {skill.skillName}{' '}
+                  {skill.subSkills &&
+                    skill.subSkills.map((subSkill) => (
+                      <ListGroup className="list-subgroup">
+                        <ListGroup.Item>{subSkill}</ListGroup.Item>
+                      </ListGroup>
+                    ))}
+                </ListGroup.Item>
+              </>
             ))}
-          </ul>
+          </ListGroup>
         </Fade>
       </Container>
     </section>
--- src/mock/data.js
+++ src/mock/data.js
@@ -77,3 +77,5 @@ export const footerData = {
     },
   ],
 };
+
+// SKILLS DATA
